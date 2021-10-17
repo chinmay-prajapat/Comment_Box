5017e0fc81303bed69dc564e8f687647f694c83b
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-10-17 17:03:05 +0530

    Basic Setup

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CommentBox from "./CommentBox";
+import ShowComments from "./ShowComments";
+const App = () => {
+  return (
+    <div>
+      <CommentBox />
+      <ShowComments />
+    </div>
+  );
+};
+export default App;
---
+++ src/components/ShowComments.js
@@ -0,0 +1,5 @@
+import React from "react";
+const ShowComments = () => {
+  return <div>Show Comments</div>;
+};
+export default ShowComments;
