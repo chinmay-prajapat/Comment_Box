e95ea5f37fe63124f478cc46e76cbc02ec1f4cb2
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-10-17 21:54:04 +0530

    text other components

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -2,7 +2,16 @@ import React from "react";
 import { shallow } from "enzyme";
 import CommentBox from "../CommentBox";
 import App from "../App";
+import CommentList from "../CommentList";
+let wrapped;
+beforeEach(() => {
+  wrapped = shallow(<App />); //Before any task run this function will run
+});
 it("shows a comment box", () => {
-  const wrapped = shallow(<App />);
   expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+it("show a comment list", () => {
+  const wrapped = shallow(<App />);
+
+  expect(wrapped.find(CommentList).length).toEqual(1);
 });
